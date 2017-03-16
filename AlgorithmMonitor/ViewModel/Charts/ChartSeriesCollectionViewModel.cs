ce03c06f7b56c25d9be453669bcd56c71a6af2e3
Anne-Mirthe Stam <anne-mirthe@zephelia.nl>
2017-03-16 16:22:31 +0100

    Series with indexes are now drawn on seperate charts. Color property for series is now respected.

---
+++ AlgorithmMonitor/ViewModel/Charts/ChartSeriesCollectionViewModel.cs
@@ -0,0 +1,61 @@
+using GalaSoft.MvvmLight;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace QuantConnect.Lean.Monitor.ViewModel.Charts
+{
+    public class ChartSeriesCollectionViewModel : ViewModelBase
+    {
+        private SeriesCollection _seriesCollection = new SeriesCollection();
+        private AxesCollection _yAxesCollection = new AxesCollection();
+        private int _index;
+
+        public ChartSeriesCollectionViewModel(int index, ChartViewModel chart)
+        {
+            _index = index;
+            _chart = chart;
+        }
+
+        private ChartViewModel _chart;
+
+        public ChartViewModel Chart
+        {
+            get { return _chart; }
+            set
+            {
+                _chart = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public AxesCollection YAxesCollection
+        {
+            get { return _yAxesCollection; }
+            set
+            {
+                _yAxesCollection = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public SeriesCollection SeriesCollection
+        {
+            get { return _seriesCollection; }
+            set
+            {
+                _seriesCollection = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int Index
+        {
+            get { return _index; }
+            set
+            {
+                _index = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
